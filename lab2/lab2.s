a641e6ff0782331db403d87dc6bfe046a5c22da2
User Miami <user@ubuntu1204.(none)>
2015-02-02 19:58:31 -0500

    add the rest of lab2

--- lab2/lab2.s
+++ lab2/lab2.s
@@ -9,6 +9,13 @@ year:
 	.text
 	.globl	calc_age
 	.type	calc_age, @function
+
+year2:
+	.long 	2050
+	.text
+	.global calc_age2
+	.type	calc_age2, @function
+	
 calc_age:
 	pushl	%ebp
 	movl	%esp, %ebp
@@ -19,6 +26,18 @@ calc_age:
 	.size	calc_age, .-calc_age
 	.section	.rodata
 	.align 4
+
+calc_age2:
+	pushl	%ebp
+	movl 	%esp, %ebp
+	movl 	year2, %eax
+	subl 	8(%ebp), %eax
+	popl 	%ebp
+	ret
+	.size	calc_age2, .-calc_age2
+	.section 	.rodata
+	.align	4
+	
 .LC0:
 	.string	"Enter the year you were born: "
 .LC1:
@@ -28,6 +47,11 @@ calc_age:
 	.string	"In the year %d, you will be %d years old\n"
 	.text
 	.globl	main
+	.type	main, @function
+.LC3:
+	.string "In the year %d, you will be %d years old\n"
+	.text
+	.global	main
 	.type	main, @function
 main:
 	pushl	%ebp
@@ -42,6 +66,7 @@ main:
 	movl	%edx, 4(%esp)
 	movl	%eax, (%esp)
 	call	__isoc99_scanf
+	;
 	movl	24(%esp), %eax
 	movl	%eax, (%esp)
 	call	calc_age
@@ -53,6 +78,19 @@ main:
 	movl	%eax, (%esp)
 	call	printf
 	movl	$0, %eax
+	;
+	movl	24(%esp), %eax
+	movl	%eax, (%esp)
+	call 	calc_age2
+	movl	%eax, 28(%esp)
+	movl 	$.LC3,	%eax
+	movl	28(%esp), %edx
+	movl	%edx, 8(%esp)
+	movl 	$2050, 4(%esp)
+	movl 	%eax, (%esp)
+	call 	printf
+	movl	$0, %eax
+	
 	leave
 	ret
 	.size	main, .-main
