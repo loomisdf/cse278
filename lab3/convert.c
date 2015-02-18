f11c3e5e33984b6c804e1e2e23f6a29e8ea9bb50
User Miami <user@ubuntu1204.(none)>
2015-02-17 23:23:46 -0500

    fix convert and make the damn thing work

--- lab3/convert.c
+++ lab3/convert.c
@@ -4,71 +4,64 @@
 
 void convert_to_octal (char input[2][33], char **output) {
   // implement your code here
-  printf("Performing the malloc\n");
-  *output = malloc(1000);
+  char* out = malloc(1000);
 
-  printf("Declaring the tmp\n");
   char tmp[4];
 
-  printf("adding the null termination\n");
   tmp[3] = '\0'; 
 
-  printf("Declaring i and j\n");
   int i;
   int j;
   int counter = 0;
-  for(j = 0; j < 1; j++) {
-    for(i = 0; i < 11; i++){
-      printf("inputy%d\n",i);
+  for(j = 0; j <= 1; j++) {
+    for(i = 0; i < (strlen(input[j]) / 3); i++){
+      
       strncpy(tmp, input[j] + (i*3), 3);
       
       if(strncmp(tmp, "000", 3) == 0) {
-	*output[counter] = 0;
+	out[counter] = '0';
       }
       else if(strncmp(tmp, "001", 3) == 0) {
-	*output[counter] = 1;
+	out[counter] = '1';
       }
       else if(strncmp(tmp, "010", 3) == 0) {
-	*output[counter] = 2;
+	out[counter] = '2';
       }
       else if(strncmp(tmp, "011", 3) == 0) {
-	*output[counter] = 3;
+	out[counter] = '3';
       }
       else if(strncmp(tmp, "100", 3) == 0) {
-	*output[counter] = 4;
+	out[counter] = '4';
       }
       else if(strncmp(tmp, "101", 3) == 0) {
-	*output[counter] = 5;
+	out[counter] = '5';
       }
       else if(strncmp(tmp, "110", 3) == 0) {
-	*output[counter] = 6;
+	out[counter] = '6';
       }
       else if(strncmp(tmp, "111", 3) == 0) {
-	*output[counter] = 7;
+	out[counter] = '7';
       }
       counter++;
     }
-    printf("place the space\n");
-    *output[counter] = ' ';
-    printf("wipe\n");
+    out[counter] = ' ';
     counter++;
   }
-  *output[counter+1] = '\0';
+  out[counter] = '\0';
+  *output = out;
 }
 
 int main() {
   char input[2][33];
-  char *output;
+  char* output;
   // implement your code here
   printf("Enter two binary strings to convert: \n");
   scanf("%s %s", input[0], input[1]);
 
   printf("The starting memory address of the input is: %p\n", input);
-  printf("the pre output\n");
   printf("The starting memory address of the output is: %p\n", output);
   
-  printf("totalling calling the method now\n");
   convert_to_octal(input, &output);
-  printf("The results in octal are: %d\n", output[0]);
+  printf("The results in octal are: %s\n", output);
   return 0;
 }
